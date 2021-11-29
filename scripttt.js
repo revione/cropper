7029148d10e4c6c071be564fc632f242946efc30
Revi <oliver.ramirez@devbase.us>
2021-11-29 15:47:47 +0100

    crop

--- scripttt.js
+++ scripttt.js
@@ -8,40 +8,14 @@ window.onload = function () {
   var container = document.querySelector(".img-container")
   var image = container.getElementsByTagName("img").item(0)
 
-  var download = document.getElementById("download")
-
   var actions = document.getElementById("actions")
 
   var options = {
     aspectRatio: 1,
     preview: ".img-preview",
-    ready: function (e) {
-      console.log(e.type)
-    },
-    cropstart: function (e) {
-      console.log(e.type, e.detail.action)
-    },
-    cropmove: function (e) {
-      console.log(e.type, e.detail.action)
-    },
-    cropend: function (e) {
-      console.log(e.type, e.detail.action)
-    },
-    crop: function (e) {
-      console.log(e.type)
-    },
-    zoom: function (e) {
-      console.log(e.type, e.detail.ratio)
-    },
   }
 
-  // console.log(" crop info -  : ", image, options)
-
   var cropper = new Cropper(image, options)
-
-  var originalImageURL = image.src
-
-  var uploadedImageType = "image/jpeg"
 
   var uploadedImageURL
 
@@ -58,11 +32,6 @@ window.onload = function () {
   ) {
     $('button[data-method="rotate"]').prop("disabled", true)
     $('button[data-method="scale"]').prop("disabled", true)
-  }
-
-  // Download
-  if (download && typeof download.download === "undefined") {
-    download.className += " disabled"
   }
 
   // Methods
@@ -103,8 +72,6 @@ window.onload = function () {
 
     cropped = cropper.cropped
 
-    console.log("data.method : ", data.method)
-
     if (data.method) {
       if (typeof data.target !== "undefined") {
         input = document.querySelector(data.target)
@@ -122,23 +89,6 @@ window.onload = function () {
         case "rotate":
           if (cropped) {
             cropper.clear()
-          }
-
-          break
-
-        case "getCroppedCanvas":
-          try {
-            data.option = JSON.parse(data.option)
-          } catch (e) {
-            console.log(e.message)
-          }
-
-          if (uploadedImageType === "image/jpeg") {
-            if (!data.option) {
-              data.option = {}
-            }
-
-            data.option.fillColor = "#fff"
           }
 
           break
@@ -166,29 +116,6 @@ window.onload = function () {
         case "scaleY":
           target.setAttribute("data-option", -data.option)
           break
-
-        case "getCroppedCanvas":
-          if (result) {
-            // Bootstrap's Modal
-            $("#getCroppedCanvasModal").modal().find(".modal-body").html(result)
-
-            if (!download.disabled) {
-              download.href = result.toDataURL(uploadedImageType)
-            }
-          }
-
-          break
-
-        case "destroy":
-          cropper = null
-
-          if (uploadedImageURL) {
-            URL.revokeObjectURL(uploadedImageURL)
-            uploadedImageURL = ""
-            image.src = originalImageURL
-          }
-
-          break
       }
 
       if (typeof result === "object" && result !== cropper && input) {
@@ -201,65 +128,30 @@ window.onload = function () {
     }
   }
 
-  document.body.onkeydown = function (event) {
-    var e = event || window.event
-
-    if (!cropper || this.scrollTop > 300) {
-      return
-    }
-
-    switch (e.keyCode) {
-      case 37:
-        e.preventDefault()
-        cropper.move(-1, 0)
-        break
-
-      case 38:
-        e.preventDefault()
-        cropper.move(0, -1)
-        break
-
-      case 39:
-        e.preventDefault()
-        cropper.move(1, 0)
-        break
-
-      case 40:
-        e.preventDefault()
-        cropper.move(0, 1)
-        break
-    }
-  }
-
   // Import image
   var inputImage = document.getElementById("inputImage")
 
-  if (URL) {
-    inputImage.onchange = function () {
-      var files = this.files
-      var file
+  inputImage.onchange = function () {
+    var files = this.files
+    var file
 
-      if (cropper && files && files.length) {
-        file = files[0]
+    if (cropper && files && files.length) {
+      file = files[0]
 
-        if (/^image\/\w+/.test(file.type)) {
-          uploadedImageType = file.type
+      if (/^image\/\w+/.test(file.type)) {
+        uploadedImageType = file.type
 
-          if (uploadedImageURL) {
-            URL.revokeObjectURL(uploadedImageURL)
-          }
-
-          image.src = uploadedImageURL = URL.createObjectURL(file)
-          cropper.destroy()
-          cropper = new Cropper(image, options)
-          inputImage.value = null
-        } else {
-          window.alert("Please choose an image file.")
+        if (uploadedImageURL) {
+          URL.revokeObjectURL(uploadedImageURL)
         }
+
+        image.src = uploadedImageURL = URL.createObjectURL(file)
+        cropper.destroy()
+        cropper = new Cropper(image, options)
+        inputImage.value = null
+      } else {
+        window.alert("Please choose an image file.")
       }
     }
-  } else {
-    inputImage.disabled = true
-    inputImage.parentNode.className += " disabled"
   }
 }
